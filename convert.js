f1162417d4d6c862bb4682d36e735749c716e1d7
myeongseob1 <kwakms123@ajou.ac.kr>
2020-04-16 11:20:14 +0900

    관리자웹 로컬서버 연동 시도

---
+++ convert.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var multer = require('multer');
+var router = express.Router();
+var date;
+var realresults;
+router.route('/').get(function(req,res){
+    res.render('start.html');
+});
+router.route('/select1').get(function(req,res){
+    res.render('select1.html');
+});
+router.route('/calender').get(function(req,res){
+    res.render('calender3.html');
+});
+router.route('/help').get(function(req,res){
+    res.render('help.html');
+});
+router.route('/amusementpark').get(function(req,res){
+    res.render('amusementpark.html');
+});
+router.route('/pastinfo').get(function(req,res){
+    res.render('pastinfo.html');
+});
+
+router.route('/everland').get(function(req,res){
+    res.render('everlandfee.html');
+});
+router.route('/lotteworld').get(function(req,res){
+    res.render('lotteworldfee.html');
+});
+router.route('/eworld').get(function(req,res){
+    res.render('eworldfee.html');
+});
+router.route('/weather').get(function(req,res){
+    res.render('weatherrealtime.html');
+});
+router.route('/date').get(function(req,res){
+    res.render('selectDate.html');
+});
+router.route('/complete').get(function(req,res){
+    res.render('complete.html');
+});
+router.route('/datareceive').get(function(req,res){
+    res.render('complete.html');
+});
+router.get('/send_data', function(req, res, next){
+    res.render('selectDate.html', { title: '' });
+});
+
+module.exports = router;
