811815e7531acac7942ac440b834dc004c269ac7
HanJungWoo <hjw9504@ajou.ac.kr>
2020-04-30 16:47:09 +0900

    reservation list DB query 추가

--- route/reservation.js
+++ route/reservation.js
@@ -7,7 +7,7 @@ router.get('/', function(req,res){
 });
 
 router.get('/list', async function(req,res){
-  let sql = 'select rd.date, l.buildingName, l.lectureRoomNum, rd.time, r.leaderId from reservation r, reservationdescription rd, lectureroom l where r.lectureRoomId=l.id'
+  let sql = 'select rd.date, l.buildingName, l.lectureRoomNum, rd.time, r.leaderId from reservation r, reservationdescription rd, lectureroom l where r.lectureRoomId=l.id and r.id=rd.reservationId'
   let recodes = await dbQuery(sql);
 
   recodes = recodes.rows;
