c11b836e07e0c7329016805afa80017b6a66f1fb
HanJungWoo <hjw9504@ajou.ac.kr>
2020-05-01 11:31:01 +0900

    Bootstrap table axios 서버 추가

--- route/lectureRoom.js
+++ route/lectureRoom.js
@@ -2,13 +2,16 @@ var express = require('express');
 var router = express.Router();
 var dbQuery = require("../database/promiseQuery.js");
 
-router.get('/', function(req,res){
-    res.render('tables-lectureroom');
+router.get('/', function(req, res) {
+  res.render('tables-lectureroom');
 });
 
-router.get('/list', function(req,res){
-  var array = [{'id': 1}, {'id': 2}]
-  res.json(array);
+router.get('/list', function(req, res) {
+  let sql = 'select rd.date, l.buildingName, l.lectureRoomNum, rd.time, r.leaderId from reservation r, reservationdescription rd, lectureroom l where r.lectureRoomId=l.id and r.id=rd.reservationId'
+  let recodes = await dbQuery(sql);
+
+  recodes = recodes.rows;
+  res.json(recodes);
 });
 
 module.exports = router;
--- route/reservation.js
+++ route/reservation.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var dbQuery = require("../database/promiseQuery.js");
 
-router.get('/', function(req,res){
-    res.render('tables-reservation');
+router.get('/', async function(req, res) {
+  res.render('tables-reservation');
 });
 
-router.get('/list', async function(req,res){
+router.get('/list', async function(req, res) {
   let sql = 'select rd.date, l.buildingName, l.lectureRoomNum, rd.time, r.leaderId from reservation r, reservationdescription rd, lectureroom l where r.lectureRoomId=l.id and r.id=rd.reservationId'
   let recodes = await dbQuery(sql);
 
